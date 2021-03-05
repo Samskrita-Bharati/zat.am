e832874fab59800ecf233fd423128c9d3f3e7a92
Deep patel <deepp2618@gmail.com>
2021-03-04 19:32:24 -0500

    Added option to print images
    Whenever the user clicks on the image, it opens the separate window to print that image

--- ctd/main.js
+++ ctd/main.js
@@ -1,18 +1,58 @@
+'use strict'
 //getting all the images
 const image_container = document.querySelector('.image-container');
 
+let address = []; 
+let img;
 
-
-for(let i =1 ; i<=21 ; ++i){
+for(let i =0 ; i<25 ; ++i){
     
     //making the img tag
-    let img = document.createElement('img');
+     img = document.createElement('img');
 
     //setting src to the image
-    let address = 'Images/' + i + '.jpg' ;
-    img.setAttribute('src', address);   // <img src='1.....21.jpg'>
+    address[i] = 'Images/' + (i+1) + '.jpg' ;
+    img.setAttribute('src', address[i]);   // <img src='1.....21.jpg'>
     img.classList.add('ctd-image');
 
     image_container.appendChild(img);
+}
+ 
+
+let images_array = document.querySelectorAll('.ctd-image');
+
+console.log(images_array);
+
+for(let i = 0 ; i<25 ; ++i){
+  
+    images_array[i].onclick = function(){
+         
+        //window.open(address[i],'Image','width=largeImage.stylewidth,height=largeImage.style.height,resizable=1');
+
+        function ImagetoPrint()
+        {
+            return "<html><head><scri"+"pt>function step1(){\n" +
+                    "setTimeout('step2()', 10);}\n" +
+                    "function step2(){window.print();window.close()}\n" +
+                    "</scri" + "pt></head><body onload='step1()'>\n" +
+                    "<img src='" + address[i] + "' /></body></html>";
+        }
+    
+        function PrintImage()
+        {
+            var Pagelink = "about:blank";
+            var pwa = window.open(Pagelink, "_new");
+            pwa.document.open();
+            pwa.document.write(ImagetoPrint(address[i]));
+            pwa.document.close();
+        }
+       PrintImage();
+        
+    
+      
+
+    }
 
 }
+
+
