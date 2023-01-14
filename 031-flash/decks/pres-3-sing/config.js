a5b003aa2438d95d628cec5760a33389d081819d
Eric Zoerner <eric.zoerner@gmail.com>
2023-01-14 08:42:58 -0800

    Fill out present tense-3rd person-singular deck With audio for first six sentences

--- 031-flash/decks/pres-3-sing/config.js
+++ 031-flash/decks/pres-3-sing/config.js
@@ -13,7 +13,7 @@ var sentences = {
     "LocalName"
   ],
   "pluralSubjectName": "Sentences",
-  "copyrightNotice": "",
+  "copyrightNotice": "Audio excerpted from Pravesha © Samskrita Bharati 2014",
   "numChoices": 4,
   "trainModeDisplay": {
     "Review": "Review",
@@ -24,50 +24,242 @@ var sentences = {
   "allSubjects": [
     {
       "subjectId": 0,
-      "audioUrl": null,
-      "unicode": "बालः धावति",
-      "latin": "Bālaḥ dhāvati",
-      "localName": "A boy runs.",
+      "audioUrl": "decks/pres-3-sing/assets/audio/bālaḥ_paṭhati.mp3",
+      "unicode": "बालः पठति ।",
+      "latin": "Bālaḥ paṭhati.",
+      "localName": "A boy reads.",
       "description": ""
     },
     {
       "subjectId": 1,
-      "audioUrl": null,
-      "unicode": "बाला गच्छति",
-      "latin": "Bālā gacchati",
-      "localName": "A girl goes.",
+      "audioUrl": "decks/pres-3-sing/assets/audio/śiṣyaḥ_namati.mp3",
+      "unicode": "शिष्यः नमति ।",
+      "latin": "Śiṣyaḥ namati.",
+      "localName": "A pupil salutes.",
       "description": ""
     },
     {
       "subjectId": 2,
-      "audioUrl": null,
-      "unicode": "अश्वः खादति",
-      "latin": "Aśvaḥ khādati",
-      "localName": "The horse eats.",
+      "audioUrl": "decks/pres-3-sing/assets/audio/agrajaḥ_vadati.mp3",
+      "unicode": "अग्रजः वदति ।",
+      "latin": "Agrajaḥ vadati.",
+      "localName": "The elder brother speaks.",
       "description": ""
     },
     {
       "subjectId": 3,
-      "audioUrl": null,
-      "unicode": "सः पचति",
-      "latin": "Saḥ pacati",
-      "localName": "He cooks.",
+      "audioUrl": "decks/pres-3-sing/assets/audio/janakaḥ_paśyati.mp3",
+      "unicode": "जनकः पश्यति ।",
+      "latin": "Janakaḥ paśyati.",
+      "localName": "Father looks on.",
       "description": ""
     },
     {
       "subjectId": 4,
-      "audioUrl": null,
-      "unicode": "अम्बा गायति",
-      "latin": "Ambā gāyati",
-      "localName": "Mother sings.",
+      "audioUrl": "decks/pres-3-sing/assets/audio/pitr̥vyaḥ_pr̥cchati.mp3",
+      "unicode": "पितृव्यः पृच्छति ।",
+      "latin": "Pitr̥vyaḥ pr̥cchati.",
+      "localName": "The paternal uncle asks.",
       "description": ""
     },
     {
       "subjectId": 5,
+      "audioUrl": "decks/pres-3-sing/assets/audio/putraḥ_gacchati.mp3",
+      "unicode": "पुत्रः गच्छति ।",
+      "latin": "Putraḥ gacchati.",
+      "localName": "The son goes.",
+      "description": ""
+    },
+    {
+      "subjectId": 6,
       "audioUrl": null,
-      "unicode": "जनकः पठति",
-      "latin": "Janakaḥ paṭhati",
-      "localName": "Father reads.",
+      "unicode": "अश्वः धावति ।",
+      "latin": "Aśvaḥ dhāvati.",
+      "localName": "The horse runs.",
+      "description": ""
+    },
+    {
+      "subjectId": 7,
+      "audioUrl": null,
+      "unicode": "वृक्षः फलति ।",
+      "latin": "Vr̥kṣaḥ phalati.",
+      "localName": "The tree bears fruit.",
+      "description": ""
+    },
+    {
+      "subjectId": 8,
+      "audioUrl": null,
+      "unicode": "सेवकः तिष्ठति ।",
+      "latin": "Sevakaḥ tiṣṭhati.",
+      "localName": "The servant stands.",
+      "description": ""
+    },
+    {
+      "subjectId": 9,
+      "audioUrl": null,
+      "unicode": "भिक्षुकः अटति ।",
+      "latin": "Bhikṣukaḥ aṭati.",
+      "localName": "The beggar wanders.",
+      "description": ""
+    },
+    {
+      "subjectId": 10,
+      "audioUrl": null,
+      "unicode": "सः निन्दति ।",
+      "latin": "Saḥ nindati.",
+      "localName": "He censures.",
+      "description": ""
+    },
+    {
+      "subjectId": 11,
+      "audioUrl": null,
+      "unicode": "अयं हसति ।",
+      "latin": "Ayaṁ hasati.",
+      "localName": "This person (mas.) laughs.",
+      "description": ""
+    },
+    {
+      "subjectId": 12,
+      "audioUrl": null,
+      "unicode": "सुतः हसति ।",
+      "latin": "Sutaḥ hasati.",
+      "localName": "Son laughs.",
+      "description": ""
+    },
+    {
+      "subjectId": 13,
+      "audioUrl": null,
+      "unicode": "अनुजा क्रीडति ।",
+      "latin": "Anujā krīḍati.",
+      "localName": "The younger sister plays.",
+      "description": ""
+    },
+    {
+      "subjectId": 14,
+      "audioUrl": null,
+      "unicode": "अग्रजा खादति ।",
+      "latin": "Agrajā khādati.",
+      "localName": "The elder sister eats.",
+      "description": ""
+    },
+    {
+      "subjectId": 15,
+      "audioUrl": null,
+      "unicode": "अम्बा पचति ।",
+      "latin": "Ambā pacati.",
+      "localName": "Mother cooks.",
+      "description": ""
+    },
+    {
+      "subjectId": 16,
+      "audioUrl": null,
+      "unicode": "रमा गायति ।",
+      "latin": "Ramā gāyati.",
+      "localName": "Ramā sings.",
+      "description": ""
+    },
+    {
+      "subjectId": 17,
+      "audioUrl": null,
+      "unicode": "बाला लिखति ।",
+      "latin": "Bālā likhati.",
+      "localName": "A girl writes.",
+      "description": ""
+    },
+    {
+      "subjectId": 18,
+      "audioUrl": null,
+      "unicode": "गङ्गा वहति ।",
+      "latin": "Gaṅgā vahati.",
+      "localName": "Ganges flows.",
+      "description": ""
+    },
+    {
+      "subjectId": 19,
+      "audioUrl": null,
+      "unicode": "सीता पिबति ।",
+      "latin": "Sītā pibati.",
+      "localName": "Sītā drinks.",
+      "description": ""
+    },
+    {
+      "subjectId": 20,
+      "audioUrl": null,
+      "unicode": "अजा चरति ।",
+      "latin": "Ajā carati.",
+      "localName": "The she-goat grazes.",
+      "description": ""
+    },
+    {
+      "subjectId": 21,
+      "audioUrl": null,
+      "unicode": "सन्ध्या भवति ।",
+      "latin": "Sandhyā bhavati.",
+      "localName": "It is evening.",
+      "description": ""
+    },
+    {
+      "subjectId": 22,
+      "audioUrl": null,
+      "unicode": "सा नयति ।",
+      "latin": "Sā nayati.",
+      "localName": "She leads/takes along.",
+      "description": ""
+    },
+    {
+      "subjectId": 23,
+      "audioUrl": null,
+      "unicode": "इयम् इच्छति ।",
+      "latin": "Iyam icchati.",
+      "localName": "This lady wants.",
+      "description": ""
+    },
+    {
+      "subjectId": 24,
+      "audioUrl": null,
+      "unicode": "पुष्पं विकसति ।",
+      "latin": "Puṣpaṁ vikasati.",
+      "localName": "Flower blooms.",
+      "description": ""
+    },
+    {
+      "subjectId": 25,
+      "audioUrl": null,
+      "unicode": "फलं पतति ।",
+      "latin": "Phalaṁ patati.",
+      "localName": "Fruit falls.",
+      "description": ""
+    },
+    {
+      "subjectId": 26,
+      "audioUrl": null,
+      "unicode": "नयनं स्फुरति ।",
+      "latin": "Nayanaṁ sphurati.",
+      "localName": "Eye throbs.",
+      "description": ""
+    },
+    {
+      "subjectId": 27,
+      "audioUrl": null,
+      "unicode": "मित्रं यच्छति ।",
+      "latin": "Mitraṁ yacchati.",
+      "localName": "Friend gives.",
+      "description": ""
+    },
+    {
+      "subjectId": 28,
+      "audioUrl": null,
+      "unicode": "जलं स्रवति ।",
+      "latin": "Jalaṁ sravati.",
+      "localName": "Water oozes.",
+      "description": ""
+    },
+    {
+      "subjectId": 29,
+      "audioUrl": null,
+      "unicode": "तत् पतति ।",
+      "latin": "It falls.",
+      "localName": "Tat patati.",
       "description": ""
     },
   ]
