9a6b15ef9dbefff3588ba2ab2153934a061cb2cc
Eric Zoerner <eric.zoerner@gmail.com>
2023-04-07 13:15:09 -0700

    [Flash] Remove references to nonexistent audio files

--- 031-flash/decks/phrases/config.js
+++ 031-flash/decks/phrases/config.js
@@ -112,7 +112,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 11,
-      "audioUrl": "decks/phrases/assets/audio/śrīman.mp3",
       "unicode": "श्रीमन्",
       "latin": "śrīman",
       "localName": "Sir",
@@ -120,7 +119,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 12,
-      "audioUrl": "decks/phrases/assets/audio/astu.mp3",
       "unicode": "अस्तु",
       "latin": "astu",
       "localName": "All right/OK",
@@ -128,7 +126,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 13,
-      "audioUrl": "decks/phrases/assets/audio/kṛpayā.mp3",
       "unicode": "कृपया",
       "latin": "kṛpayā",
       "localName": "Please",
@@ -136,7 +133,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 14,
-      "audioUrl": "decks/phrases/assets/audio/cintā-māstu.mp3",
       "unicode": "चिन्ता मास्तु",
       "latin": "cintā māstu",
       "localName": "Don't worry",
@@ -144,7 +140,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 15,
-      "audioUrl": "decks/phrases/assets/audio/kṣamyatām.mp3",
       "unicode": "क्षम्यताम्",
       "latin": "kṣamyatām",
       "localName": "Excuse me",
@@ -152,7 +147,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 16,
-      "audioUrl": "decks/phrases/assets/audio/punaḥ-milāmaḥ.mp3",
       "unicode": "पुनः मिलामः",
       "latin": "punaḥ milāmaḥ",
       "localName": "See you again",
@@ -160,7 +154,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 17,
-      "audioUrl": "decks/phrases/assets/audio/sādhu-sādhu.mp3",
       "unicode": "साधु साधु",
       "latin": "sādhu sādhu",
       "localName": "Very good",
@@ -168,7 +161,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 18,
-      "audioUrl": "decks/phrases/assets/audio/uttamam.mp3",
       "unicode": "उत्तमम्",
       "latin": "uttamam",
       "localName": "Good",
@@ -176,7 +168,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 19,
-      "audioUrl": "decks/phrases/assets/audio/bahu-samīcīnam.mp3",
       "unicode": "बहु समीचीनम्",
       "latin": "bahu samīcīnam",
       "localName": "Very fine",
@@ -184,7 +175,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 20,
-      "audioUrl": "decks/phrases/assets/audio/śubhāśayāḥ.mp3",
       "unicode": "शुभाशयाः",
       "latin": "śubhāśayāḥ",
       "localName": "Best wishes",
@@ -192,7 +182,6 @@ var संस्कृतठ्यवहारः = {
     },
     {
       "subjectId": 21,
-      "audioUrl": "decks/phrases/assets/audio/abhinandanāni.mp3",
       "unicode": "अभिनन्दनानि",
       "latin": "abhinandanāni",
       "localName": "Congratulations",
