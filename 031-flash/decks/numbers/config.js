f09f91c8330c5523bae0c38dbab967a04b16b695
Eric "सन्तोष" Zoerner <eric.zoerner@proton.me>
2023-10-27 01:16:19 -0700

    [Flash] New decks for digits and names of numbers

---
+++ 031-flash/decks/numbers/config.js
@@ -0,0 +1,209 @@
+var numbers = {
+  "deckId": "numbers",
+  "deckTitle": "Sanskrit Numbers",
+  "landingPage": "index.html",
+  "initialScript": "Unicode",
+  "scriptCanBeSet": true,
+  "autoPlayCanBeSet": true,
+  "mainPartFontSize": "32pt",
+  "showAudioWithUrNameQuiz": false,
+  "showAudioWithLocalNameQuiz": true,
+  "showDescription": false,
+  "trainingModes": [
+    "Review",
+    "Urname",
+    "LocalName"
+  ],
+  "pluralSubjectName": "Numbers",
+  "copyrightNotice": "",
+  "numChoices": 4,
+  "trainModeDisplay": {
+    "Review": "Review",
+    "Urname": "Sanskrit",
+    "LocalName": "English",
+  },
+  "scriptHeading": "Sanskrit Script",
+  "allSubjects": [
+    {
+      "subjectId": 1,
+      "audioUrl": "decks/numbers/assets/audio/00000001.mp3",
+      "unicode": "एकम्",
+      "latin": "ēkam",
+      "localNames": ["one", "1"],
+      "description": ""
+    },
+    {
+      "subjectId": 2,
+      "audioUrl": "decks/numbers/assets/audio/00000002.mp3",
+      "unicode": "द्वे",
+      "latin": "dvē",
+      "localNames": ["two", "2"],
+      "description": ""
+    },
+    {
+      "subjectId": 3,
+      "audioUrl": "decks/numbers/assets/audio/00000003.mp3",
+      "unicode": "त्रीणि",
+      "latin": "trīṇi",
+      "localNames": ["three", "3"],
+      "description": ""
+    },
+    {
+      "subjectId": 4,
+      "audioUrl": "decks/numbers/assets/audio/00000004.mp3",
+      "unicode": "चत्वारि",
+      "latin": "catvāri",
+      "localNames": ["four", "4"],
+      "description": ""
+    },
+    {
+      "subjectId": 5,
+      "audioUrl": "decks/numbers/assets/audio/00000005.mp3",
+      "unicode": "पञ्च",
+      "latin": "pañca",
+      "localNames": ["five", "5"],
+      "description": ""
+    },
+    {
+      "subjectId": 6,
+      "audioUrl": "decks/numbers/assets/audio/00000006.mp3",
+      "unicode": "षट्",
+      "latin": "ṣaṭ",
+      "localNames": ["six", "6"],
+      "description": ""
+    },
+    {
+      "subjectId": 7,
+      "audioUrl": "decks/numbers/assets/audio/00000007.mp3",
+      "unicode": "सप्त",
+      "latin": "sapta",
+      "localNames": ["seven", "7"],
+      "description": ""
+    },
+    {
+      "subjectId": 8,
+      "audioUrl": "decks/numbers/assets/audio/00000008.mp3",
+      "unicode": "अष्ट",
+      "latin": "aṣṭa",
+      "localNames": ["eight", "8"],
+      "description": ""
+    },
+    {
+      "subjectId": 9,
+      "audioUrl": "decks/numbers/assets/audio/00000009.mp3",
+      "unicode": "नव",
+      "latin": "nava",
+      "localNames": ["nine", "9"],
+      "description": ""
+    },
+    {
+      "subjectId": 10,
+      "audioUrl": "decks/numbers/assets/audio/00000010.mp3",
+      "unicode": "दश",
+      "latin": "daśa",
+      "localNames": ["ten", "10"],
+      "description": ""
+    },
+    {
+      "subjectId": 20,
+      "audioUrl": "decks/numbers/assets/audio/00000020.mp3",
+      "unicode": "विंशतिः",
+      "latin": "viṁśatiḥ",
+      "localNames": ["twenty", "20"],
+      "description": ""
+    },
+    {
+      "subjectId": 30,
+      "audioUrl": "decks/numbers/assets/audio/00000030.mp3",
+      "unicode": "त्रिंशत्",
+      "latin": "triṁśat",
+      "localNames": ["thirty", "30"],
+      "description": ""
+    },
+    {
+      "subjectId": 40,
+      "audioUrl": "decks/numbers/assets/audio/00000040.mp3",
+      "unicode": "चत्वारिंशत्",
+      "latin": "catvāriṁśat",
+      "localNames": ["forty", "40"],
+      "description": ""
+    },
+    {
+      "subjectId": 50,
+      "audioUrl": "decks/numbers/assets/audio/00000050.mp3",
+      "unicode": "पञ्चाशत्",
+      "latin": "pañcāśat",
+      "localNames": ["fifty", "50"],
+      "description": ""
+    },
+    {
+      "subjectId": 60,
+      "audioUrl": "decks/numbers/assets/audio/00000060.mp3",
+      "unicode": "षष्टिः",
+      "latin": "ṣaṣṭiḥ",
+      "localNames": ["sixty", "60"],
+      "description": ""
+    },
+    {
+      "subjectId": 70,
+      "audioUrl": "decks/numbers/assets/audio/00000070.mp3",
+      "unicode": "सप्ततिः",
+      "latin": "saptatiḥ",
+      "localNames": ["seventy", "70"],
+      "description": ""
+    },
+    {
+      "subjectId": 80,
+      "audioUrl": "decks/numbers/assets/audio/00000080.mp3",
+      "unicode": "अशीतिः",
+      "latin": "aśītiḥ",
+      "localNames": ["eighty", "80"],
+      "description": ""
+    },
+    {
+      "subjectId": 90,
+      "audioUrl": "decks/numbers/assets/audio/00000090.mp3",
+      "unicode": "नवतिः",
+      "latin": "navatiḥ",
+      "localNames": ["ninety", "90"],
+      "description": ""
+    },
+    {
+      "subjectId": 100,
+      "audioUrl": "decks/numbers/assets/audio/00000100.mp3",
+      "unicode": "शतम्",
+      "latin": "śatam",
+      "localNames": ["one hundred", "100"],
+      "description": ""
+    },
+    {
+      "subjectId": 1000,
+      "audioUrl": "decks/numbers/assets/audio/00001000.mp3",
+      "unicode": "सहस्रम्",
+      "latin": "sahasram",
+      "localNames": ["one thousand", "1000"],
+      "description": ""
+    },
+    {
+      "subjectId": 100000,
+      "audioUrl": "decks/numbers/assets/audio/00100000.mp3",
+      "unicode": "लक्षम्",
+      "latin": "lakṣam",
+      "localNames": ["one hundred thousand", "1,00,000"],
+      "description": ""
+    },
+    {
+      "subjectId": 10000000,
+      "audioUrl": "decks/numbers/assets/audio/10000000.mp3",
+      "unicode": "कोटिः",
+      "latin": "koṭiḥ",
+      "localNames": ["ten million", "1,00,00,000"],
+      "description": ""
+    },
+  ]
+}
+
+var app = Elm.Main.init({
+  node: document.getElementById('elm'),
+  flags: numbers
+});
