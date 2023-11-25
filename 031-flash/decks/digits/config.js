b0bb7c3dc240e1bf2de4bf8c7d1add0374b7f237
Eric "सन्तोष" Zoerner <santosha.samadhi@proton.me>
2023-11-25 14:31:40 -0800

    [Flash] Numbers 11-100 (#29)
    [Flash] Add cards and audio for numbers 11-100

--- 031-flash/decks/digits/config.js
+++ 031-flash/decks/digits/config.js
@@ -33,7 +33,6 @@ var digits = {
       "latin": "",
       "localNames": ["1"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 2,
@@ -42,7 +41,6 @@ var digits = {
       "latin": "",
       "localNames": ["2"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 3,
@@ -51,7 +49,6 @@ var digits = {
       "latin": "",
       "localNames": ["3"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 4,
@@ -60,7 +57,6 @@ var digits = {
       "latin": "",
       "localNames": ["4"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 5,
@@ -69,7 +65,6 @@ var digits = {
       "latin": "",
       "localNames": ["5"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 6,
@@ -78,7 +73,6 @@ var digits = {
       "latin": "",
       "localNames": ["6"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 7,
@@ -87,7 +81,6 @@ var digits = {
       "latin": "",
       "localNames": ["7"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 8,
@@ -96,7 +89,6 @@ var digits = {
       "latin": "",
       "localNames": ["8"],
       "description": "",
-      "group": "1-9",
     },
     {
       "subjectId": 9,
@@ -105,111 +97,6 @@ var digits = {
       "latin": "",
       "localNames": ["9"],
       "description": "",
-      "group": "1-9",
-    },
-    {
-      "subjectId": 10,
-      "audioUrl": "decks/digits/assets/audio/00000010.mp3",
-      "unicode": "१०",
-      "latin": "",
-      "localNames": ["10"],
-      "description": "",
-    },
-    {
-      "subjectId": 20,
-      "audioUrl": "decks/digits/assets/audio/00000020.mp3",
-      "unicode": "२०",
-      "latin": "",
-      "localNames": ["20"],
-      "description": ""
-    },
-    {
-      "subjectId": 30,
-      "audioUrl": "decks/digits/assets/audio/00000030.mp3",
-      "unicode": "३०",
-      "latin": "",
-      "localNames": ["30"],
-      "description": ""
-    },
-    {
-      "subjectId": 40,
-      "audioUrl": "decks/digits/assets/audio/00000040.mp3",
-      "unicode": "४०",
-      "latin": "",
-      "localNames": ["40"],
-      "description": ""
-    },
-    {
-      "subjectId": 50,
-      "audioUrl": "decks/digits/assets/audio/00000050.mp3",
-      "unicode": "५०",
-      "latin": "",
-      "localNames": ["50"],
-      "description": ""
-    },
-    {
-      "subjectId": 60,
-      "audioUrl": "decks/digits/assets/audio/00000060.mp3",
-      "unicode": "६०",
-      "latin": "",
-      "localNames": ["60"],
-      "description": ""
-    },
-    {
-      "subjectId": 70,
-      "audioUrl": "decks/digits/assets/audio/00000070.mp3",
-      "unicode": "७०",
-      "latin": "",
-      "localNames": ["70"],
-      "description": ""
-    },
-    {
-      "subjectId": 80,
-      "audioUrl": "decks/digits/assets/audio/00000080.mp3",
-      "unicode": "८०",
-      "latin": "",
-      "localNames": ["80"],
-      "description": ""
-    },
-    {
-      "subjectId": 90,
-      "audioUrl": "decks/digits/assets/audio/00000090.mp3",
-      "unicode": "९०",
-      "latin": "",
-      "localNames": ["90"],
-      "description": ""
-    },
-    {
-      "subjectId": 100,
-      "audioUrl": "decks/digits/assets/audio/00000100.mp3",
-      "unicode": "१००",
-      "latin": "",
-      "localNames": ["100"],
-      "description": ""
-    },
-    {
-      "subjectId": 1000,
-      "audioUrl": "decks/digits/assets/audio/00001000.mp3",
-      "unicode": "१०००",
-      "latin": "",
-      "localNames": ["1000"],
-      "description": ""
-    },
-    {
-      "subjectId": 100000,
-      "audioUrl": "decks/digits/assets/audio/00100000.mp3",
-      "unicode": "१,००,०००",
-      "latin": "",
-      "localNames": ["1,00,000"],
-      "description": ""
-    },
-    {
-      "subjectId": 10000000,
-      "audioUrl": "decks/digits/assets/audio/10000000.mp3",
-      "unicode": "१,००,००,०००",
-      "latin": "",
-      "localNames": ["1,00,00,000"],
-      "description": ""
     },
   ]
 }
