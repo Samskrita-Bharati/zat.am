033a63ba53681026dc65c14fda1848c920dfbb3c
Karanveer <143984706+ksmasoun@users.noreply.github.com>
2024-02-25 19:11:45 -0500

    Game 047 - Sarp Leela

---
+++ 047-sl/scripts/snake.js
@@ -0,0 +1,74 @@
+import { getInputDirection } from "./input.js";
+
+export const SNAKE_SPEED = 5;
+const snakeBody = [{ x: 11, y: 11 }];
+let newSegments = 0;
+
+export function update() {
+  addSegments();
+
+  const inputDirection = getInputDirection();
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
+}
+
+export function draw(gameBoard) {
+  snakeBody.forEach((segment, index) => {
+    const snakeElement = document.createElement("div");
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    
+    if (index === 0) {
+      snakeElement.classList.add("snake-head");
+      snakeElement.innerHTML = `
+        <div class="head-circle">
+          <span class="snake-number">${toSanskritNumber(snakeBody.length)}</span>
+        </div>
+      `;
+    } else {
+      snakeElement.classList.add("snake-body");
+    }
+
+    gameBoard.appendChild(snakeElement);
+  });
+}
+
+export function expandSnake(amount) {
+  newSegments += amount;
+}
+
+export function onSnake(position, { ignoreHead = false } = {}) {
+  return snakeBody.some((segment, index) => {
+    if (ignoreHead && index === 0) return false;
+    return equalPositions(segment, position);
+  });
+}
+
+export function getSnakeHead() {
+  return snakeBody[0];
+}
+
+export function snakeIntersection() {
+  return onSnake(snakeBody[0], { ignoreHead: true });
+}
+
+function equalPositions(pos1, pos2) {
+  return pos1.x === pos2.x && pos1.y === pos2.y;
+}
+
+function addSegments() {
+  for (let i = 0; i < newSegments; i++) {
+    snakeBody.push({ ...snakeBody[snakeBody.length - 1] });
+  }
+
+  newSegments = 0;
+}
+
+function toSanskritNumber(number) {
+  const sanskritNumbers = ["०", "१", "२", "३", "४", "५", "६", "७", "८", "९"];
+  return number.toString().split("").map(digit => sanskritNumbers[digit]).join("");
+}
