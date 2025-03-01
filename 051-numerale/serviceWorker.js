6bbf5137e49a08dc43c7a2733e39f3ac4b3eda3a
pd2 <its.pdp@gmail.com>
2025-03-01 15:48:26 +0000

    Create serviceWorker.js

---
+++ 051-numerale/serviceWorker.js
@@ -0,0 +1,24 @@
+const staticDev = "numerals"
+const service_worker_version = "v1.0"
+const assets = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDev).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
