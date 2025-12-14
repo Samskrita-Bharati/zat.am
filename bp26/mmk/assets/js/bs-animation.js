73d4aa0f44f2119f9f7bdc3f2dda2e35304752d4
thakkaha <harsh.thakkar@sheridancollege.ca>
2025-12-14 13:10:56 -0500

    bp26-v7

--- bp26/mmk/assets/js/bs-animation.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('[data-bs-hover-animate]')
-		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
-		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
-});
