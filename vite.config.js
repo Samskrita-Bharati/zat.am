8cd5cfa3bbf0a34b65a990789296060d642ad924
arhamCh24 <arhammu@sheridancollege.ca>
2026-01-28 14:09:17 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,33 @@
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   base: "/zat.am/",
+//   build: {
+//     outDir: "dist",
+//     rollupOptions: {
+//       input: "index24.html",
+//     },
+//   },
+//   server: {
+//     open: "/index24.html",
+//   },
+// });
+
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "./",
+  base: "/zat.am/",
   build: {
     outDir: "dist",
-  },
-  server: {
-    open: "/index24.html",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: "index24.html",
+        login: "auth/login.html",
+        signup: "auth/signup.html",
+        profile: "auth/profile.html",
+        privacy: "legal/privacy.html",
+      },
+    },
   },
 });
